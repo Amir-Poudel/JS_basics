f190112d21ba64e89461a2ad56f5d6cde8c766b5
amir.r10 <amir.poudel2000@gmail.com>
2026-06-23 13:41:39 +0545

    checking

--- 17.git.js
+++ 17.git.js
@@ -1,2 +1,13 @@
 //* git branch -> list all local branches
 //this is the example line added..
+//!git and github
+
+//* git status
+//*git add <file path>
+//* git add .
+//*git commit -m "commit_message"
+//*git push origin <branch_name>
+
+//?remote
+//*git remote -v
+//* git remote add origin <remote_URL>
